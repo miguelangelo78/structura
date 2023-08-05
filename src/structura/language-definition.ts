32d8e730ef13d9afab2b8adeefbed6d689932b88
Miguel Santos <santosmiguel25@gmail.com>
2023-08-05 23:50:15 +0100

    Remove language definition regarding comments

--- src/structura/language-definition.ts
+++ src/structura/language-definition.ts
@@ -47,9 +47,6 @@ If a CALL command still exists, the PRINT command must be ignored.
 
 Each keyword that is followed by ':' is considered a command.
 
-You must ignore any Structura code that is in between the characters '/*' and '*/'. This is a comment.
-You must ignore single line comments that start with '//'.
-
 Variables can be created using the keyword 'VAR'. Variables can be assigned using the keyword 'SET'. Variables can be referenced using the keyword 'GET'. Variables can be deleted using the keyword 'DEL'.
 
 Variables can hold anything. They can hold a string, a number, an object, a topic of conversation, a symbolic reference, or just about anything.
